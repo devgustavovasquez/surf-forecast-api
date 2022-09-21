530e33c54337759c8877c306ebcdb58dd6fd8a19
devgustavovasquez <gustavovasquez2002@gmail.com>
2022-09-20 22:03:15 -0300

    feat: encrypt password with bcrypt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { BaseModel } from '.';
+import AuthService from '@src/services/auth';
 
 export interface User extends BaseModel {
   name: string;
@@ -43,5 +44,18 @@ schema.path('email').validate(
   'already exists in the database',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (err) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User = mongoose.model<User>('User', schema);
