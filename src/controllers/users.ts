2a598c05b7af012f830c6af94f07c27e3d3b75d1
devgustavovasquez <gustavovasquez2002@gmail.com>
2022-09-23 19:56:53 -0300

    feat: implements authenticate JWT for users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
 
@@ -14,5 +15,21 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error as Error);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return;
+    }
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      return;
+    }
+
+    const token = AuthService.generateToken(user.toJSON());
+
+    res.status(200).send({ token });
   }
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export default class AuthService {
   public static async hashPassword(
@@ -13,5 +14,11 @@ export default class AuthService {
     hashedPassword: string
   ): Promise<boolean> {
     return await bcrypt.compare(passord, hashedPassword);
+  }
+
+  public static generateToken(payload: object): string {
+    return jwt.sign(payload, 'test', {
+      expiresIn: 10000,
+    });
   }
 }
