cd6d3a56ee5c28249bd17b0ec001cc2fa0e7f557
devgustavovasquez <gustavovasquez2002@gmail.com>
2022-09-23 20:39:25 -0300

    refact: use app config in token jwt config

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
   public static async hashPassword(
@@ -17,8 +18,8 @@ export default class AuthService {
   }
 
   public static generateToken(payload: object): string {
-    return jwt.sign(payload, 'test', {
-      expiresIn: 10000,
+    return jwt.sign(payload, config.get('App.auth.key'), {
+      expiresIn: config.get('App.auth.tokenExpiresIn'),
     });
   }
 }
